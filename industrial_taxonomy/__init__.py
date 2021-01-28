35aafc6524ec80833e3269287104b8544ed7ca5f
bishax <alex.bishop@nesta.org.uk>
2021-01-28 19:43:29 +0000

    Setup Nesta Data Science cookiecutter (#3)
    Closes #1

---
+++ industrial_taxonomy/__init__.py
@@ -0,0 +1,34 @@
+import logging
+import logging.config
+import yaml
+from pathlib import Path
+from typing import Optional
+
+
+def get_log_config(file_path: Path) -> Optional[dict]:
+    if file_path.exists():
+        with open(file_path, "rt") as f:
+            return yaml.load(f.read(), Loader=yaml.FullLoader)
+
+
+# Define project base directory
+project_dir = Path(__file__).resolve().parents[1]
+
+# Define log output locations
+info_out = str(project_dir / "info.log")
+error_out = str(project_dir / "errors.log")
+
+# Read log config file
+log_config_file = project_dir / "logging.yaml"
+logging_config = get_log_config(log_config_file)
+if logging_config:
+    logging.config.dictConfig(logging_config)
+
+# Define module logger
+logger = logging.getLogger(__name__)
+
+# Model config
+with open(
+    project_dir / "industrial_taxonomy" / "model_config.yaml", "rt"
+) as f:
+    config = yaml.safe_load(f.read())
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='industrial_taxonomy',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ESCoE project to explore shortcomings of SIC taxonomy and build a new industrial taxonomy using business website data.',
+    author='Nesta',
+    license='MIT',
+)
