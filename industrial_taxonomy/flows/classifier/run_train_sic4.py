9bb97196850d7e67d5624a888b5350fb659f9039
George Richardson <georgerichardson@posteo.net>
2021-04-30 13:00:49 +0100

    workaround for different classifier config ids

--- industrial_taxonomy/flows/classifier/run_train_sic4.py
+++ industrial_taxonomy/flows/classifier/run_train_sic4.py
@@ -9,8 +9,9 @@ from industrial_taxonomy.flows.classifier.classifier_utils import create_org_dat
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    FLOW_ID = "sic4_classifier"
-    task_config = config["flows"][FLOW_ID]
+    FLOW_ID = "classifier_train"
+    CONFIG_ID = "sic4_classifier"
+    task_config = config["flows"][CONFIG_ID]
     output_dir = task_config["config"]["training_args"]["output_dir"]
     task_config["config"]["training_args"]["output_dir"] = str(project_dir / output_dir)
     flow_config = {
@@ -19,7 +20,7 @@ if __name__ == "__main__":
             '--config': json.dumps(task_config['config'])
             }
 
-    flow_file = Path(__file__).resolve().parents[0] / f"classifier_train.py"
+    flow_file = Path(__file__).resolve().parents[0] / f"{FLOW_ID}.py"
     run_id = execute_flow(
             flow_file,
             flow_config,
@@ -27,4 +28,4 @@ if __name__ == "__main__":
             )
 
     flow_config["run_id"] = run_id
-    update_model_config(["flows", FLOW_ID], task_config)
+    update_model_config(["flows", CONFIG_ID], task_config)
