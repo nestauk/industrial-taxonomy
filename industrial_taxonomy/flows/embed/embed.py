fb906d9aa132f40357128df713c64d880fca5512
George Richardson <georgerichardson@posteo.net>
2021-05-03 17:53:01 +0100

    add embedding flow

---
+++ industrial_taxonomy/flows/embed/embed.py
@@ -0,0 +1,40 @@
+from sentence_transformers import SentenceTransformer
+
+from metaflow import FlowSpec, step, Parameter, Run
+from sentence_transformers import SentenceTransformer
+
+from industrial_taxonomy.flows.classifier.classifier_utils import (
+       sort_by_char_len) 
+
+
+class Embedder(FlowSpec):
+    model = Parameter(
+            "model",
+            help="Model version to use",
+            type=str,
+            )
+    preproc_run_id = Parameter(
+            "preproc_flow_run_id",
+            help="Run ID of the preprocessing flow",
+            type=int
+            )
+
+    def start(self):
+        self.preproc_run = Run(
+                f'SicPreprocess/{self.preproc_run_id}')
+        self.next(self.embed)
+
+    def embed(self):
+        model = SentenceTransformer(self.model)
+        texts = (t['text'] for t in preproc_run.data.org_data)
+
+        self.encodings = model.encode(texts)
+
+        self.next(self.end)
+
+    def end(self):
+        pass
+
+if __name__ == "__main__":
+    Embedder()
+
