4d14ba797f8345f4594c38280a6b8b602d2554cd
bishax <alex.bishop@nesta.org.uk>
2021-05-12 12:53:54 +0100

    Refactor and improve topsbm analysis

--- industrial_taxonomy/scripts/topsbm_sector_heterogeneity.py
+++ industrial_taxonomy/scripts/topsbm_sector_heterogeneity.py
@@ -1,106 +1,121 @@
-# %%
-"""Look at heterogeneity of SIC codes using TopSBM topics.
-
-TODO: LOTS OF REFACTORING - this is essentially a notebook
-"""
-from collections import defaultdict
-from itertools import product
+"""Look at heterogeneity of SIC codes using TopSBM topics."""
+import logging
+from typing import Dict, Optional
 
 import altair as alt
-import numpy as np
 import pandas as pd
 import toolz.curried as t
 import tqdm
-from IPython.display import display
-from research_daps.flows.topsbm.sbmtm import sbmtm as Sbmtm
-
 from scipy.stats import entropy
 
 from industrial_taxonomy.getters.sic import level_lookup
 from industrial_taxonomy.getters.topsbm import get_topsbm_v2
 from industrial_taxonomy.queries.sector import get_glass_SIC_sectors
 from industrial_taxonomy.utils.altair_s3 import export_chart
-
-# %%
-
-
-@t.curry
-def group_topsbm_by_sector(
-    sector: pd.DataFrame, L: int, SIC_level: int, model: Sbmtm
-) -> pd.DataFrame:
-    """Group TopSBM topic by SIC code.
-
-    Args:
-      sector: SIC lookup for Glass orgs
-      L: TopSBM model level
-      SIC_level: Number of digits of SIC code to aggregate at
-
-    Returns:
-      Rows: sector
-      Columns: cluster index
-      Values: prob. of a description in sector X being in cluster Y
-    """
-    # _, _, dict_groups = model.get_groups(L)
-    dict_groups = model.get_groups(L)
-    p_td_d = dict_groups["p_tw_d"]
-
-    cluster_prob = pd.DataFrame(  # Rows: organisations | Columns: clusters | Values:
-        p_td_d.T, index=list(map(int, model.documents))
-    )
-    return (
-        sector.merge(
-            cluster_prob,
-            left_on="org_id",
-            right_index=True,
-        )
-        .melt(  # Columns: `SIC5_code`, `variable` (cluster number), `value` (cluster probability)
-            id_vars=["SIC5_code"],
-            value_vars=np.arange(p_td_d.shape[0]),
-        )
-        # Slice SIC code to `SIC_level`
-        .assign(sector=lambda x: x.SIC5_code.str.slice(0, SIC_level))
-        .drop(["SIC5_code"], axis=1)
-        .pipe(
-            lambda x: pd.pivot_table(
-                x,
-                index="sector",
-                columns="variable",
-                values="value",
-                aggfunc=np.mean,
-            )
-        )
-    )
+from industrial_taxonomy.utils.econ_geo import location_quotient
+from industrial_taxonomy.scripts.topsbm_utils import group_topsbm_by_sector as group_topsbm_by_sector_
 
 
 alt.data_transformers.disable_max_rows()
-# %%
+logger = logging.getLogger(__name__)
 
-SIC_levels = [2, 3, 4]
+SIC_LEVEL = 4
 
-model = get_topsbm_v2()
-sector = get_glass_SIC_sectors()
-sic_entropy: dict = defaultdict(lambda: defaultdict(lambda: {}))
-topic_activity_by_sector: dict = defaultdict(lambda: defaultdict(lambda: {}))
+TopWords = Dict[str, str]
 
-# %%
 
-for L, SIC_level in tqdm.tqdm(product(range(0, 3), SIC_levels)):
-    topic_activity_by_sector[SIC_level][L] = group_topsbm_by_sector(
-        sector, L, SIC_level, model
+def _melt(data, SIC_level):
+    """Melt and format data indexed by sector across variables."""
+    return (
+        data.melt(ignore_index=False)  # Melt for altair
+        .rename_axis(index="sector")
+        .reset_index()
+        # Labels
+        .assign(
+            sector_name=lambda x: x.sector
+            + ": "
+            + x.sector.map(level_lookup(SIC_level))
+        )
+        .dropna()  # Currently 98000 and 99999 codes
     )
-    sic_entropy[SIC_level][L] = topic_activity_by_sector[SIC_level][L].apply(
-        entropy, axis=1
+
+
+def plot_sector_topic_activity(
+    topic_activity_by_sector: pd.DataFrame,
+    sectors: pd.DataFrame,
+    top_words: Optional[TopWords] = None,
+    value_name: str = "Topic activity",
+) -> alt.Chart:
+    """Bar chart of topic activity in `sectors`.
+
+    Args:
+        TODO
+
+    Returns:
+        Chart
+    """
+    L = len(sectors[0])
+    return (
+        alt.Chart(
+            topic_activity_by_sector.loc[sectors]
+            .melt(ignore_index=False, value_name=value_name)
+            .pipe(
+                lambda df: df
+                if not top_words
+                else df.assign(top_words=lambda x: x.variable.map(top_words))
+            )
+            .reset_index()
+            .assign(
+                sector_name=lambda x: x.sector + ": " + x.sector.map(level_lookup(L))
+            )
+            .dropna()
+        )
+        .mark_bar()
+        .encode(
+            y=alt.Y(value_name, title=value_name),
+            x=alt.X(
+                "variable:N",
+                title="Topic number",
+                sort="-y",
+            ),
+            color=alt.Color(
+                "sector_name:N",
+                title="Sector name",
+                scale=alt.Scale(scheme="category20b"),
+            ),
+            tooltip=[
+                alt.Tooltip("variable", title="Topic number"),
+                alt.Tooltip(value_name, title="Topic activity"),
+                alt.Tooltip("sector_name:N", title="Sector"),
+                alt.Tooltip("top_words:N", title="Top words"),
+            ],
+        )
     )
 
-# %%
 
+def plot_sector_entropy_interactive(
+    sector_entropy: pd.DataFrame, top_words: Optional[TopWords] = None
+) -> alt.Chart:
+    """Minimap bar chart of SIC entropy.
 
-def plot_interactive(data):
-    """Minimap bar chart of SIC entropy."""
+    Args:
+        sector_entropy: Index is SIC codes, columns are entropy across model levels.
+        top_words: Keys are SIC codes, values are string list of top words.
+
+    Returns:
+        Chart
+    """
+    SIC_level = len(sector_entropy.index[0])
 
     label_size = 200
     brush = alt.selection_interval(encodings=["y"])
-    base = alt.Chart(data.pipe(_melt)).mark_bar()
+    base = alt.Chart(
+        sector_entropy.pipe(_melt, SIC_level).pipe(
+            lambda df: df
+            if not top_words
+            else df.assign(top_words=lambda x: x.sector.map(top_words))
+        )
+    ).mark_bar()
     sort = alt.EncodingSortField(field="value", op="max", order="descending")
 
     minimap = (
@@ -130,53 +145,46 @@ def plot_interactive(data):
                 alt.Tooltip("sector_name", title="Sector"),
                 alt.Tooltip("variable", title="Model hierarchy level"),
                 alt.Tooltip("value", title="entropy"),
+                alt.Tooltip(
+                    "top_words", title="Top words of most differentiated topic"
+                ),
             ],
         )
         .transform_filter(brush)
         .add_selection(alt.selection_single())
-        .properties(
-            # title=f"SIC level {SIC_level}",
-            height=600
-        )
+        .properties(height=600)
     )
 
     return chart | minimap
 
 
-def _melt(data):
-    """Melt and format data indexed by sector across variables."""
-    return (
-        data.melt(ignore_index=False)  # Melt for altair
-        .rename_axis(index="sector")
-        .reset_index()
-        # Labels
-        .assign(
-            sector_name=lambda x: x.sector
-            + ": "
-            + x.sector.map(level_lookup(SIC_level))
-        )
-        .dropna()  # Currently 98000 and 99999 codes
-    )
+def plot_sector_entropy_static(sector_entropy: pd.DataFrame, n: int = 10) -> alt.Chart:
+    """Bar chart of SIC entropy for top `n` and bottom `n` sectors.
 
+    Args:
+        sector_entropy: Index is SIC codes, columns are entropy across model levels.
+        n: How many top or bottom sectors to show.
 
-def plot_static(data, n=10):
-    """Minimap bar chart of SIC entropy."""
+    Returns:
+        Chart
+    """
 
     label_size = 200
+    SIC_level = len(sector_entropy.index[0])
 
     top_data = (
-        data.assign(agg_value=lambda x: x.max(axis=1))
+        sector_entropy.assign(agg_value=lambda x: x.max(axis=1))
         .sort_values("agg_value", ascending=False)
         .head(n)
         .drop(["agg_value"], axis=1)
-        .pipe(_melt)
+        .pipe(_melt, SIC_level)
     )
     bottom_data = (
-        data.assign(agg_value=lambda x: x.max(axis=1))
+        sector_entropy.assign(agg_value=lambda x: x.max(axis=1))
         .sort_values("agg_value", ascending=False)
         .tail(n)
         .drop(["agg_value"], axis=1)
-        .pipe(_melt)
+        .pipe(_melt, SIC_level)
     )
 
     sort = alt.EncodingSortField(field="value", op="max", order="descending")
@@ -211,90 +219,80 @@ def plot_static(data, n=10):
     ).resolve_axis(x="shared")
 
 
-for SIC_level in tqdm.tqdm(SIC_levels):
-    data = pd.concat(
+if __name__ == "__main__":
+    group_topsbm_by_sector = group_topsbm_by_sector_(cluster_type="word")
+
+    model = get_topsbm_v2()
+    sector = get_glass_SIC_sectors()
+
+    model_levels = [0, 1, 2]
+    model_level = 1
+
+    # %%
+    # Pre-calc topic activity and entropy
+    topic_activity_by_sector: dict = {
+        L: group_topsbm_by_sector(sector, L, SIC_LEVEL, model)
+        for L in tqdm.tqdm(model_levels)
+    }
+    sector_entropy = pd.concat(
         [
-            data.to_frame(f"Level {level}")
-            for level, data in sic_entropy[SIC_level].items()
+            data.apply(entropy, axis=1).to_frame(f"Level {level}")
+            for level, data in topic_activity_by_sector.items()
         ],
         axis=1,
     )
+    # %%
+    # Top words of topic that most differentiated sector - model level `model_level`
+    n_words = 20
+    top_words = {
+        k: ", ".join(map(t.first, v))
+        for k, v in model.topics(model_level, n_words).items()
+    }
+    top_words_of_sector = (
+        topic_activity_by_sector[model_level]
+        .pipe(location_quotient)
+        .idxmax(axis=1)
+        .map(top_words)
+        .pipe(dict)
+    )
 
+    # %%
+    # Plot entropy of SIC codes across `model_levels`
     export_chart(
-        plot_interactive(data),
-        static_alt_chart=plot_static(data),
-        key=f"topsbm/SIC{SIC_level}_entropy",
+        plot_sector_entropy_interactive(sector_entropy, top_words_of_sector),
+        static_alt_chart=plot_sector_entropy_static(sector_entropy),
+        key=f"topsbm/SIC{SIC_LEVEL}_entropy",
     )
 
-# %%
+    # %%
+    # Look at the topics of the most heterogeneous sectors
+    n_top_sectors = 22  # 9999 and 7499 don't count
 
-
-def sector_topic_activity(topic_activity_by_sector, sectors):
-    """Bar chart of topic activity in `sectors`."""
-    L = len(sectors[0])
-    return (
-        alt.Chart(
-            topic_activity_by_sector.loc[sectors]
-            .melt(ignore_index=False)
-            .reset_index()
-            .assign(
-                sector_name=lambda x: x.sector + ": " + x.sector.map(level_lookup(L))
-            )
-            .dropna()
+    top_sectors = (
+        sector_entropy.assign(
+            agg_value=lambda x: x.max(axis=1),  # How we agg across model levels
+            sector=lambda x: x.index.str.slice(0, SIC_LEVEL),
         )
-        .mark_bar()
-        .encode(
-            y=alt.Y("value:Q", title="Topic activity"),
-            x=alt.X("variable:N", title="Topic number"),
-            color="sector_name:N",
-            tooltip=[
-                alt.Tooltip("variable", title="Topic number"),
-                alt.Tooltip("value", title="Topic activity"),
-                alt.Tooltip("sector_name:N", title="Sector"),
-            ],
+        .sort_values("agg_value", ascending=False)
+        .head(n_top_sectors)
+        .drop(["agg_value"], axis=1)
+        .sector.values
+    )
+
+    value_name = "Topic activity (fraction of mean)"
+
+    chart = (
+        plot_sector_topic_activity(
+            topic_activity_by_sector[model_level].pipe(location_quotient),
+            top_sectors,
+            top_words,
+            value_name,
         )
+        # .transform_joinaggregate(lq=f"sum({value_name})", groupby=["variable"])
+        # .transform_filter((alt.datum.lq > 15))
+        .properties(title="Most heterogeneous", width=800)
+        .resolve_legend("independent")
+        .resolve_scale(color="independent")
     )
-
-
-n = 10
-SIC_level = 2
-L = 1
-
-top_sectors = (
-    data.assign(
-        agg_value=lambda x: x.max(axis=1),
-        sector=lambda x: x.index.str.slice(0, SIC_level),
-    )
-    .sort_values("agg_value", ascending=False)
-    .head(n)
-    .drop(["agg_value"], axis=1)
-    .sector.values
-)
-bottom_sectors = (
-    data.assign(
-        agg_value=lambda x: x.max(axis=1),
-        sector=lambda x: x.index.str.slice(0, SIC_level),
-    )
-    .sort_values("agg_value", ascending=False)
-    .tail(n)
-    .drop(["agg_value"], axis=1)
-    .sector.values
-)
-
-
-chart = (
-    (
-        sector_topic_activity(
-            topic_activity_by_sector[SIC_level][L], top_sectors
-        ).properties(title="Most heterogeneous", width=800)
-        & sector_topic_activity(
-            topic_activity_by_sector[SIC_level][L], bottom_sectors
-        ).properties(title="Most homogeneous", width=800)
-    )
-    .resolve_legend("independent")
-    .resolve_scale(color="independent")
-)
-display(chart)
-
-export_chart(chart, key=f"topsbm/SIC{SIC_level}_entropy_topicdist_L{L}")
-# %%
+    export_chart(chart, key=f"topsbm/SIC{SIC_LEVEL}_hetero_topicdist_L{model_level}")
+    # %%
---
+++ industrial_taxonomy/scripts/topsbm_utils.py
@@ -0,0 +1,57 @@
+"""Topsbm analysis helpers."""
+import numpy as np
+import pandas as pd
+import toolz.curried as t
+from research_daps.flows.topsbm.sbmtm import sbmtm as Sbmtm
+
+
+@t.curry
+def group_topsbm_by_sector(
+    sector: pd.DataFrame, L: int, SIC_level: int, model: Sbmtm, cluster_type: str
+) -> pd.DataFrame:
+    """Group TopSBM cluster by SIC code.
+
+    Args:
+      sector: SIC lookup for Glass orgs
+      L: TopSBM model level
+      SIC_level: Number of digits of SIC code to aggregate at
+      model: TopSBM model
+      cluster_type: "document" (i.e. clusters of orgs) or "word" (i.e. topics)
+
+    Returns:
+      Rows: sector
+      Columns: cluster index
+      Values: prob. of a description in sector X being in cluster Y
+    """
+    if cluster_type not in {"document", "word"}:
+        raise ValueError("`cluster_type` must be 'document' or 'word'")
+    # _, _, dict_groups = model.get_groups(L)
+    dict_groups = model.get_groups(L)
+    p_tX_d = dict_groups["p_td_d" if cluster_type == "document" else "p_tw_d"]
+
+    cluster_prob = pd.DataFrame(  # Rows: organisations | Columns: clusters | Values:
+        p_tX_d.T, index=list(map(int, model.documents))
+    )
+    return (
+        sector.merge(
+            cluster_prob,
+            left_on="org_id",
+            right_index=True,
+        )
+        .melt(  # Columns: `SIC5_code`, `variable` (cluster number), `value` (cluster probability)
+            id_vars=["SIC5_code"],
+            value_vars=np.arange(p_tX_d.shape[0]),
+        )
+        # Slice SIC code to `SIC_level`
+        .assign(sector=lambda x: x.SIC5_code.str.slice(0, SIC_level))
+        .drop(["SIC5_code"], axis=1)
+        .pipe(
+            lambda x: pd.pivot_table(
+                x,
+                index="sector",
+                columns="variable",
+                values="value",
+                aggfunc=np.mean,
+            )
+        )
+    )
