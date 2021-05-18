e213840976ba6238a964533cd3d84c2e7f17cd6f
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2021-05-18 09:50:29 +0100

    add more getters to processing

--- industrial_taxonomy/getters/processing.py
+++ industrial_taxonomy/getters/processing.py
@@ -1,7 +1,8 @@
 # Scripts to get processing and processed files
-
+import pandas as pd
 import yaml
 import industrial_taxonomy
+import pickle
 
 project_dir = industrial_taxonomy.project_dir
 
@@ -10,3 +11,13 @@ def get_config(element):
     with open(f"{project_dir}/industrial_taxonomy/model_config.yaml", "r") as infile:
         out = yaml.safe_load(infile)[element]
     return out
+
+def get_table(name):
+    
+    return pd.read_csv(f"{project_dir}/data/processed/{name}.csv")
+
+def get_networks():
+
+    with open(f"{project_dir}/data/processed/sector_networks.p", "rb") as infile:
+        nets = pickle.load(infile)
+    return nets
